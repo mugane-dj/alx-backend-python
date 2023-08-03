b5b25461ec074d27292dd59053cc76ceb0d52c23
mugane-dj <muganedev25@gmail.com>
2023-08-03 11:33:36 +0300

    0x2

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,17 @@
+import math
+"""
+This module contains a type-annotated function floor which
+takes a float n as argument and returns the floor of the float.
+"""
+
+
+def floor(n: float) -> float:
+    """
+    The function `floor` takes a float number as input and returns
+    the largest integer less than or equal to the input.
+
+    :param n: A float number that we want to find the floor value of
+    :type n: float
+    :return: the floor value of the input number.
+    """
+    return math.floor(n)
