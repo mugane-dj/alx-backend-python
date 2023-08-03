4fee4c975ace5e48f375d8098df1ce69d547bc6e
mugane-dj <muganedev25@gmail.com>
2023-08-03 11:38:21 +0300

    0x3

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,15 @@
+"""
+This module contains  a type-annotated function to_str that takes a float n
+as argument and returns the string representation of the float.
+"""
+
+
+def to_str(n: float) -> str:
+    """
+    The function `to_str` converts a float number to a string representation.
+
+    :param n: a float number
+    :type n: float
+    :return: a string representation of the input float number.
+    """
+    return n.__str__()
