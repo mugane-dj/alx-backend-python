3c39ea2c86933c35f5062d59c5a51571cd31dc92
mugane-dj <muganedev25@gmail.com>
2023-08-03 11:34:53 +0300

    0x2

--- 0x00-python_variable_annotations/2-floor.py
+++ 0x00-python_variable_annotations/2-floor.py
@@ -5,7 +5,7 @@ takes a float n as argument and returns the floor of the float.
 """
 
 
-def floor(n: float) -> float:
+def floor(n: float) -> int:
     """
     The function `floor` takes a float number as input and returns
     the largest integer less than or equal to the input.
