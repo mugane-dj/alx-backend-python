ceeee92cf68fd5a7df01f26029b0cc90575532f3
mugane-dj <muganedev25@gmail.com>
2023-08-08 11:00:10 +0300

    0x1

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -3,10 +3,11 @@
 async_comprehension - implements a coroutine that uses an async
 generator to return 10 random float numbers using async comprehensing
 """
+from typing import List
 async_generator = __import__('0-async_generator').async_generator
 
 
-async def async_comprehension():
+async def async_comprehension() -> List[float]:
     """
     The function `async_comprehension` returns a list of floats
     obtained from an asynchronous generator.
