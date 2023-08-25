66145214e9db8ac4c24b27b03efca21315672248
mugane-dj <muganedev25@gmail.com>
2023-08-25 11:22:40 +0300

    0x1

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -19,6 +19,18 @@ class TestAccessNestedMap(unittest.TestCase):
     def test_access_nested_map(self, nested_map, path, expected_result):
         self.assertEqual(access_nested_map(nested_map, path), expected_result)
 
+    @parameterized.expand([
+        ({}, ("a",), KeyError),
+        ({"a": 1}, ("a", "b"), KeyError)
+    ])
+    def test_access_nested_map_exception(self, nested_map, path,
+                                         expected_result):
+        if issubclass(expected_result, Exception):
+            with self.assertRaises(expected_result):
+                access_nested_map(nested_map, path)
+        else:
+            self.assertEqual(access_nested_map(nested_map, path))
+
 
 if __name__ == '__main__':
     unittest.main()
