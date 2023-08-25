0e7b5b9126410e3e096bd413681c65e759fbb1eb
mugane-dj <muganedev25@gmail.com>
2023-08-25 14:49:21 +0300

    0x2

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -3,74 +3,75 @@
 Parameterize a unit test
 """
 import unittest
+from unittest.mock import Mock, patch
 from parameterized import parameterized
-access_nested_map = __import__('utils').access_nested_map
-get_json = __import__('utils').get_json
-memoize = __import__('utils').memoize
+from typing import Dict, Tuple, Union
+
+access_nested_map = __import__("utils").access_nested_map
+get_json = __import__("utils").get_json
+memoize = __import__("utils").memoize
 
 
 class TestAccessNestedMap(unittest.TestCase):
     """
     Test utils function access_nested_map
     """
-    @parameterized.expand([
+
+    @parameterized.expand(
+        [
             ({"a": 1}, ("a",), 1),
             ({"a": {"b": 2}}, ("a",), {"b": 2}),
-            ({"a": {"b": 2}}, ("a", "b"), 2)
-    ])
-    def test_access_nested_map(self, nested_map, path, expected_result):
+            ({"a": {"b": 2}}, ("a", "b"), 2),
+        ]
+    )
+    def test_access_nested_map(
+        self,
+        nested_map: Dict,
+        path: Tuple[str],
+        expected_result: Union[Dict, int],
+    ) -> None:
+        """
+        Tests access_nested_map returns output from mapping with provided sequence
+        """
         self.assertEqual(access_nested_map(nested_map, path), expected_result)
 
-    @parameterized.expand([
-        ({}, ("a",), KeyError),
-        ({"a": 1}, ("a", "b"), KeyError)
-    ])
-    def test_access_nested_map_exception(self, nested_map, path,
-                                         expected_result):
+    @parameterized.expand(
+        [({}, ("a",), KeyError), ({"a": 1}, ("a", "b"), KeyError)]
+    )
+    def test_access_nested_map_exception(
+        self, nested_map: Dict, path: Tuple[str], expected_result: Exception
+    ) -> None:
+        """
+        Tests access_nested_map raises exception
+        """
         if issubclass(expected_result, Exception):
             with self.assertRaises(expected_result):
                 access_nested_map(nested_map, path)
 
 
-# class TestGetJson(unittest.TestCase):
-#     """
-#     Tests for get_json function
-#     """
+class TestGetJson(unittest.TestCase):
+    """
+    Tests for get_json function
+    """
 
-#     @parameterized.expand([
-#         ("http://example.com", {"payload": True}),
-#         ("http://holberton.io", {"payload": False})
-#     ])
-#     @unittest.mock.patch('requests.get')
-#     def test_get_json(self, test_url, test_payload, mock_get):
-#         mock_res = unittest.mock.Mock()
-#         mock_res.json.return_value = test_payload
-#         mock_get.return_value = mock_res
+    @parameterized.expand(
+        [
+            ("http://example.com", {"payload": True}),
+            ("http://holberton.io", {"payload": False}),
+        ]
+    )
+    def test_get_json(self, test_url: str, test_payload: Dict) -> None:
+        """
+        Tests get_json can be mocked successfully
+        """
+        mock_res = Mock()
+        mock_res.json.return_value = test_payload
 
-#         res = get_json(test_url)
-
-#         self.assertEqual(res, test_payload)
-#         mock_res.json.assert_called_once()
-#         mock_get.assert_called_once_with(test_url)
-
-
-class TestClass:
-
-    def a_method(self):
-        return 42
-
-    @memoize
-    def a_property(self):
-        return self.a_method()
-
-    @unittest.mock.patch('utils.cache', {})
-    def test_memoize(self):
-        for _ in range(2):
-            result = self.a_property()
-
-        self.assertEquals(self.a_method(), result)
-        self.a_method.assert_called_once()
+        with patch("requests.get") as mock_get:
+            mock_get.return_value = mock_res
+            self.assertEqual(get_json(test_url), test_payload)
+            mock_get.assert_called_once_with(test_url)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()
