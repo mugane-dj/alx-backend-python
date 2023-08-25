c4e0ace0c3e3272b55dd04320fcbe052d812d64e
mugane-dj <muganedev25@gmail.com>
2023-08-25 12:27:21 +0300

    0x2

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -19,70 +19,67 @@ class TestAccessNestedMap(unittest.TestCase):
             ({"a": {"b": 2}}, ("a", "b"), 2)
     ])
     def test_access_nested_map(self, nested_map, path, expected_result):
-        """
-        Test access_nested_map function happy path
-        """
         self.assertEqual(access_nested_map(nested_map, path), expected_result)
 
-    @parameterized.expand([
-        ({}, ("a",), KeyError),
-        ({"a": 1}, ("a", "b"), KeyError)
-    ])
-    def test_access_nested_map_exception(self, nested_map, path,
-                                         expected_result):
-        """
-        Test access_nested_map function for exception
-        """
-        if issubclass(expected_result, Exception):
-            with self.assertRaises(expected_result):
-                access_nested_map(nested_map, path)
+#     @parameterized.expand([
+#         ({}, ("a",), KeyError),
+#         ({"a": 1}, ("a", "b"), KeyError)
+#     ])
+#     def test_access_nested_map_exception(self, nested_map, path,
+#                                          expected_result):
+#         """
+#         Test access_nested_map function for exception
+#         """
+#         if issubclass(expected_result, Exception):
+#             with self.assertRaises(expected_result):
+#                 access_nested_map(nested_map, path)
 
 
-class TestGetJson(unittest.TestCase):
-    """
-    Tests for get_json function
-    """
+# class TestGetJson(unittest.TestCase):
+#     """
+#     Tests for get_json function
+#     """
 
-    @unittest.mock.patch('utils.requests.get')
-    def test_get_json(self, mock_get):
-        """
-        Test that utils.get_json returns the expected result.
-        """
-        mock_res = unittest.mock.Mock()
-        mock_res.json.return_value = {'data': 'mock_res'}
-        mock_get.return_value = mock_res
+#     @unittest.mock.patch('utils.requests.get')
+#     def test_get_json(self, mock_get):
+#         """
+#         Test that utils.get_json returns the expected result.
+#         """
+#         mock_res = unittest.mock.Mock()
+#         mock_res.json.return_value = {'data': 'mock_res'}
+#         mock_get.return_value = mock_res
 
-        res = get_json('http://api.example.com/')
+#         res = get_json('http://api.example.com/')
 
-        self.assertEqual(res, {'data': 'mock_res'})
-        mock_get.assert_called_once_with('http://api.example.com/')
+#         self.assertEqual(res, {'data': 'mock_res'})
+#         mock_get.assert_called_once_with('http://api.example.com/')
 
 
-class TestClass:
+# class TestClass:
 
-    def a_method(self):
-        """
-        A dummy method
-        """
-        return 42
+#     def a_method(self):
+#         """
+#         A dummy method
+#         """
+#         return 42
 
-    @memoize
-    def a_property(self):
-        """
-        A memoized property should only call the method once
-        """
-        return self.a_method()
+#     @memoize
+#     def a_property(self):
+#         """
+#         A memoized property should only call the method once
+#         """
+#         return self.a_method()
 
-    @unittest.mock.patch('utils.cache', {})
-    def test_memoize(self):
-        """
-        Test memoization
-        """
-        for _ in range(2):
-            result = self.a_property()
+#     @unittest.mock.patch('utils.cache', {})
+#     def test_memoize(self):
+#         """
+#         Test memoization
+#         """
+#         for _ in range(2):
+#             result = self.a_property()
 
-        self.assertEquals(self.a_method(), result)
-        self.a_method.assert_called_once()
+#         self.assertEquals(self.a_method(), result)
+#         self.a_method.assert_called_once()
 
 
 if __name__ == '__main__':
