4ea50525c2a7a43a0c8c88d7c338e2da65695b62
mugane-dj <muganedev25@gmail.com>
2023-08-28 17:34:04 +0300

    0x7

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -92,7 +92,7 @@ class TestGithubOrgClient(unittest.TestCase):
     @parameterized.expand(
         [
             ({"license": {"key": "bsd-3-clause"}}, "bsd-3-clause", True),
-            ({"license": {"key": "bsl-1.0"}}, "bsd-3-clause", False),
+            ({"license": {"key": "apache-2.0"}}, "bsd-3-clause", False),
         ]
     )
     def test_has_license(
