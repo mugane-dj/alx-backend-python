4ac33a6e5feadc673503e656e567025b71138064
mugane-dj <muganedev25@gmail.com>
2023-08-28 10:14:13 +0300

    0x3

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -74,5 +74,23 @@ class TestGetJson(unittest.TestCase):
             mock_get.assert_called_once_with(test_url)
 
 
+class TestMemoize(unittest.TestCase):
+    def test_memoize(self):
+        class TestClass:
+            def a_method(self):
+                return 42
+
+            @memoize
+            def a_property(self):
+                return self.a_method()
+
+        with patch.object(
+            TestClass, "a_method", return_value=lambda: 42
+        ) as mock_method:
+            self.assertEqual(TestClass().a_property(), 42)
+            self.assertEqual(TestClass().a_property(), 42)
+            mock_method.assert_called_once
+
+
 if __name__ == "__main__":
     unittest.main()
