274308391b1298314d4d4a4bde06586ae027b735
mugane-dj <muganedev25@gmail.com>
2023-08-28 16:41:55 +0300

    0x5

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 import unittest
-from unittest.mock import MagicMock, patch
+from unittest.mock import MagicMock, patch, PropertyMock
 from typing import Dict
 from parameterized import parameterized
 
@@ -20,9 +20,22 @@ class TestGithubOrgClient(unittest.TestCase):
         """
         Test memoized func org
         """
-        client = GithubOrgClient(org)
+        instance = GithubOrgClient(org)
         mocked_func.return_value = MagicMock(return_value=res)
-        self.assertEqual(client.org(), res)
+        self.assertEqual(instance.org(), res)
         mocked_func.assert_called_once_with(
             "https://api.github.com/orgs/{}".format(org)
         )
+
+    def test_public_repos_url(self):
+        instance = GithubOrgClient("google")
+        mock_prop = PropertyMock(
+            return_value={
+                "repos_url": "https://api.github.com/orgs/google/repos"
+            }
+        )
+        with patch.object(instance.__class__, "org", mock_prop):
+            self.assertEqual(
+                instance._public_repos_url,
+                "https://api.github.com/orgs/google/repos",
+            )
